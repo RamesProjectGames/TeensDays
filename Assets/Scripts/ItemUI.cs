45c199fd4043fe62b96b4a2c14e9f109c61d8c37
Rama Zukri <ramazukri2@gmail.com>
2025-10-02 16:05:47 +0700

    push

--- Assets/Scripts/ItemUI.cs
+++ Assets/Scripts/ItemUI.cs
@@ -1,13 +1,14 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ItemUI : MonoBehaviour
 {
-
-
+    public static ItemUI Instance;
 
     [Header("Panel Shop Confirmation")]
     public GameObject confirmationPanel;
@@ -17,22 +18,43 @@ public class ItemUI : MonoBehaviour
     private int price;
     private int priceMoney;
 
-
+    [Header("Shop Panel")]
     public Image currImage;
     public TextMeshProUGUI itemNameText;
     public TextMeshProUGUI itemDescText;
     public TextMeshProUGUI priceText;
+    public Image rarityFrame;
+    public string rarityType;
+    public string itemId;
     public Button buyButton;
 
     public Sprite imageDiamond;
     public Sprite imageMoney;
     public bool isDiamondPayment;
-    public void SetItem(string name, string desc, int priceValue, int _priceMoney, bool _isDiamondPayment)
+    public bool isAlreadyPurchased;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void SetItem(string _itemId,string name, string desc, int priceValue, int _priceMoney, bool _isDiamondPayment, string rarity)
     {
         itemNameText.text = name;
         itemDescText.text = desc;
         isDiamondPayment = _isDiamondPayment;
+        rarityType = rarity;
+        itemId = _itemId;
         var img = currImage.sprite;
+
+        switch (rarity.ToLower())
+        {
+            case "rare": rarityFrame.color = Color.blue; break;
+            case "epic": rarityFrame.color = new Color(0.6f, 0f, 1f); break; // ungu
+            case "legend": rarityFrame.color = new Color(1f, 0.5f, 0f); break; // oranye
+            case "mythic": rarityFrame.color = Color.red; break;
+            default: rarityFrame.color = Color.white; break;
+        }
 
         if (isDiamondPayment)
         {
@@ -48,49 +70,63 @@ public class ItemUI : MonoBehaviour
         }   
 
         buyButton.onClick.RemoveAllListeners();
-        buyButton.onClick.AddListener(() => BuyItem());
+
+        // ✅ simpan dulu ke variable lokal agar tidak tertimpa
+        string thisItemId = _itemId;
+        buyButton.onClick.AddListener(() => BuyItem(thisItemId)); //Debug.Log(_itemId)
     }
 
-    void BuyItem()
+    void BuyItem(string itemId)
     {
-        int playerCurrency = PlayerPrefs.GetInt("Diamond", 0);
-        int _playerCurrency = PlayerPrefs.GetInt("Money", 0);
+        Debug.Log(itemId);
 
-        if (isDiamondPayment)
+        string path = Path.Combine(Application.streamingAssetsPath, "shop_items.json");
+        string jsonData = File.ReadAllText(path);
+        string wrappedJson = "{ \"items\": " + jsonData + "}";
+        ShopItemList itemList = JsonUtility.FromJson<ShopItemList>(wrappedJson);
+
+        for (int i = 0; i < itemList.items.Length; i++)
         {
-            if (playerCurrency >= price)
+            ShopItem item = itemList.items[i];
+            if (itemId == item.itemId)
             {
-                GameManager.Instance.currDiamond -= price;
-                PlayerPrefs.SetInt("Diamond", playerCurrency);
-                Debug.Log("Berhasil beli item: " + itemNameText.text);
-                completePurchased.SetActive(true);
-
-
-            }
-            else
-            {
-                Debug.Log("Currency tidak cukup!");
-                cancelPurchased.SetActive(true);
+                if (item.isDiamondPayment)
+                {
+                    int playerCurrency = GameManager.Instance.currDiamond;
+                    if (playerCurrency >= item.price)
+                    {
+                        playerCurrency -= item.price;
+                        GameManager.Instance.currDiamond = playerCurrency;
+                        PlayerPrefs.SetInt("Diamond", playerCurrency);
+                        Debug.Log("Berhasil beli item: " + item.name);
+                        completePurchased.SetActive(true);
+                    }
+                    else
+                    {
+                        Debug.Log("Currency tidak cukup!");
+                        cancelPurchased.SetActive(true);
+                    }
+                }
+                else
+                {
+                    int _playerCurrency = GameManager.Instance.currMoney;
+                    if (_playerCurrency >= item.priceMoney)
+                    {
+                        _playerCurrency -= item.priceMoney;
+                        GameManager.Instance.currMoney = _playerCurrency;
+                        PlayerPrefs.SetInt("Money", _playerCurrency);
+                        Debug.Log("Berhasil beli item: " + item.name);
+                        completePurchased.SetActive(true);
+                    }
+                    else
+                    {
+                        Debug.Log("Currency tidak cukup!");
+                        cancelPurchased.SetActive(true);
+                    }
+                }
+                PlayerPrefs.Save();
+                break;
             }
         }
-        else
-        {
-            if (_playerCurrency >= priceMoney)
-            {
-                GameManager.Instance.currMoney -= priceMoney;
-                PlayerPrefs.SetInt("Diamond", _playerCurrency);
-                Debug.Log("Berhasil beli item: " + itemNameText.text);
-                completePurchased.SetActive(true);
-
-
-            }
-            else
-            {
-                Debug.Log("Currency tidak cukup!");
-                cancelPurchased.SetActive(true);
-            }
-        }
-
-            PlayerPrefs.Save();
     }
 }
