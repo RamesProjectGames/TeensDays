2eed59fd5140c7fd40f9b94642f16c2cdcc42944
Rama Zukri <ramazukri2@gmail.com>
2026-02-20 19:07:07 +0700

    push UI

--- Assets/UiQuestAnim.cs
+++ Assets/UiQuestAnim.cs
@@ -11,6 +11,8 @@ public class UiQuestAnim : MonoBehaviour
     public float pointOfY;
 
     private float currentAngle = 0f;
+
+    [SerializeField] private Camera mainCamera;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,29 +22,37 @@ public class UiQuestAnim : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Hitung arah ke player
-        Vector3 targetDirection = player.position - transform.position;
+        Quaternion rotation = mainCamera.transform.rotation;
+        transform.LookAt(transform.position + rotation * Vector3.forward, rotation * Vector3.up);
 
-        // Hitung rotasi yang diinginkan, tetapi hanya pada sumbu X
-        Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+        //// Hitung arah ke player
+        //Vector3 targetDirection = player.position - transform.position;
 
-        // Ambil sudut rotasi saat ini dan target hanya pada sumbu X
-        float currentAngle = transform.eulerAngles.x; // Rotasi pada sumbu X
-        float targetAngle = targetRotation.eulerAngles.x; // Rotasi target pada sumbu X
+        //// Hitung rotasi yang diinginkan, tetapi hanya pada sumbu X
+        //Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
 
-        // Menghitung selisih sudut
-        float angleDifference = Mathf.DeltaAngle(currentAngle, targetAngle);
+        //// Ambil sudut rotasi saat ini dan target hanya pada sumbu X
+        //float currentAngle = transform.eulerAngles.x; // Rotasi pada sumbu X
+        //float targetAngle = targetRotation.eulerAngles.x; // Rotasi target pada sumbu X
 
-        // Membatasi rotasi agar tidak melebihi maxRotationAngle
-        if (Mathf.Abs(angleDifference) > maxRotasion)
-        {
-            targetAngle = currentAngle + Mathf.Sign(angleDifference) * maxRotasion;
-        }
+        //// Menghitung selisih sudut
+        //float angleDifference = Mathf.DeltaAngle(currentAngle, targetAngle);
 
-        // Interpolasi rotasi objek ke target secara bertahap pada sumbu X
-        float newAngle = Mathf.LerpAngle(currentAngle, targetAngle, maxRotasion * Time.deltaTime);
+        //// Membatasi rotasi agar tidak melebihi maxRotationAngle
+        //if (Mathf.Abs(angleDifference) > maxRotasion)
+        //{
+        //    targetAngle = currentAngle + Mathf.Sign(angleDifference) * maxRotasion;
+        //}
 
-        // Set hanya rotasi pada sumbu X, tanpa mengubah sumbu Y dan Z
-        transform.rotation = Quaternion.Euler(newAngle, transform.eulerAngles.y, transform.eulerAngles.z);
+        //// Interpolasi rotasi objek ke target secara bertahap pada sumbu X
+        //float newAngle = Mathf.LerpAngle(currentAngle, targetAngle, maxRotasion * Time.deltaTime);
+
+        //// Set hanya rotasi pada sumbu X, tanpa mengubah sumbu Y dan Z
+        //transform.rotation = Quaternion.Euler(newAngle, transform.eulerAngles.y, transform.eulerAngles.z);
+    }
+
+    private void _Reset()
+    {
+        mainCamera = Camera.main;
     }
 }
