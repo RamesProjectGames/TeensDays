98130a360323824d95ca648ed67e2e415b7fd3bd
RamesProjectGames <ramesprojectgames@gmail.com>
2026-06-17 12:58:47 +0700

    Change Load Scene from Level Manager to use string

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -14,6 +14,7 @@ public class LocalTime
 }
 public class LevelManager : MonoBehaviour
 {
+    public int minClass;
     public Button[] levelButtons;
     public LocalTime[] levelRetries;
     public GameObject[] bgSpritesLocked;
@@ -49,6 +50,15 @@ public class LevelManager : MonoBehaviour
         SceneManager.sceneLoaded += OnSceneLoaded;
 
         SceneManager.LoadScene(levelId);
+    }
+    public void OpenLevel(string levelName)
+    {
+        string prevSceneName = SceneManager.GetActiveScene().name;
+        int prevSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
+
+        SceneManager.LoadScene(levelName);
     }
 
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
@@ -90,7 +100,7 @@ public class LevelManager : MonoBehaviour
         int unlockedLevel = 1;
         if (GameManager.Instance != null && GameManager.Instance.playerData != null)
         {
-            unlockedLevel = GameManager.Instance.playerData.unlockedLevel;
+            unlockedLevel = GameManager.Instance.playerData.unlockedLevel - minClass;
         }
 
         for (int i = 0; i < levelButtons.Length; i++)
@@ -117,7 +127,7 @@ public class LevelManager : MonoBehaviour
             var levelRetriesNumbers = GameManager.Instance.playerData.levelRetries.list;
             for (int i = 0; i < levelRetries.Length; i++)
             {
-                int playerClass = levelRetries[i].classId;
+                int playerClass = levelRetries[i].classId - minClass;
                 int retries = levelRetriesNumbers[playerClass];
                 levelRetries[i].timeText.text = $"{retries} / 3";
                 Debug.Log($"Level {i + 1} retries: {retries}");
